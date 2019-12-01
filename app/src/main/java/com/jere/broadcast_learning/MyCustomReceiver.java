32069c823c8d67545e6f8b3e678aac53d9ded4d9
JereChen <jerechen11@gmail.com>
2019-12-01 15:07:03 +0800

    code review

--- app/src/main/java/com/jere/broadcast_learning/MyCustomReceiver.java
+++ app/src/main/java/com/jere/broadcast_learning/MyCustomReceiver.java
@@ -13,6 +13,7 @@ public class MyCustomReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "MyCustomReceiver Running!", Toast.LENGTH_SHORT).show();
+        //将 intent 中的信息解析出来，得到发送这信息。
         Bundle bundle = intent.getExtras();
         if (bundle != null) {
             String author = bundle.getString(MainActivity.SEND_BROADCAST_AUTHOR_KEY);
